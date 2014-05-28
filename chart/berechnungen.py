5c75177a50ca2c25fac2edb6370cad0234742828
Martin Virtel <martin.virtel@gmail.com>
2014-05-28 12:56:14 +0200

    Initial

---
+++ chart/berechnungen.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+import math 
+
+print "laut meckpom"
+print "Breite lt. %d" % (math.sqrt(21194/9744)*45,)
+print "Höhe lt. %d" % (math.sqrt(21194/9744)*38,)
+
+# http://www.berlin.de/berlin-im-ueberblick/zahlenfakten/index.de.html
+
+
+print "laut nrw"
+a=34100;
+s=271*58
+print "Breite lt. %d" % (math.sqrt(a/s)*45,)
+print "Höhe lt. %d" % (math.sqrt(a/s)*38,)
+
+
+print "tf"
+km=math.sqrt(66*65)
+print "Breite Tempelhof: %s" % (km*2.094)
+print "Breite Fußballfeld: %s" % (km*0.105)
+print "Höhe Fußballfeld: %s" % (km*0.068)
---
+++ push-code.sh
@@ -0,0 +1,1 @@
+rsync -av chart/ nyx.opendatacloud.de:/var/www/apps.opendatacity.de/agrarstruktur/
